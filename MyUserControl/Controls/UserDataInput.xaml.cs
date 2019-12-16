0f807be4e8933fc43e4b67ba25fdb9d73414c2c5
Joakim Wassberg <joakim@arthead.se>
2019-12-16 13:19:38 +0100

    Initial Commit

---
+++ MyUserControl/Controls/UserDataInput.xaml.cs
@@ -0,0 +1,26 @@
+﻿using MyUserControl.Models;
+using Windows.UI.Xaml.Controls;
+
+
+namespace MyUserControl.Controls
+{
+    public sealed partial class UserDataInput : UserControl
+    {
+        private User _user;
+
+        public User UserData
+        {
+            get { return _user; }
+            set { 
+                _user = value;
+                UserName.Text = _user?.Name ?? "";
+                UserAge.Text = _user?.Age.ToString() ?? "0";
+            }
+        }
+
+        public UserDataInput()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ MyUserControl/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MyUserControl.Models;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace MyUserControl
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            var u1 = new User { Name = "Billy Bob", Age = 34 };
+            var u2 = new User { Name = "Anna Bob", Age = 23 };
+            var u3 = new User { Name = "David Bob", Age = 45 };
+            var u4 = new User { Name = "Sue Bob", Age = 67 };
+            List<User> users = new List<User> { u1, u2, u3, u4 };
+            foreach (var user in users)
+            {
+                Users.Items.Add(user);
+            }
+        }
+
+        private void Users_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var userItem = ((ListView)sender).SelectedItem;
+            var user = userItem as User;
+            UserDataControl.UserData = user;
+            //UserName.Text = user.Name;
+            //UserAge.Text = user.Age.ToString();
+        }
+    }
+}
